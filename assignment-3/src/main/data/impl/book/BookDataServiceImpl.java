b19b80fa025f0dd44fdc50ea4e646e71bc11bc39
hanqi <rhanqtl@gmail.com>
2019-01-27 16:01:57 +0800

    assignment-3: 假装写完了

--- assignment-3/src/main/data/impl/book/BookDataServiceImpl.java
+++ assignment-3/src/main/data/impl/book/BookDataServiceImpl.java
@@ -1,15 +1,39 @@
 package main.data.impl.book;
 
 import main.common.book.Book;
+import main.common.book.Level;
 import main.data.service.book.BookDataService;
 
-import java.util.List;
+import java.util.*;
 
 public class BookDataServiceImpl implements BookDataService {
 
-    private static final Book[] BOOKS = new Book[] {
+    private static final Set<Book> BOOKS = new HashSet<>();
 
-    };
+    static {
+        BOOKS.add(new Book("9787536692930", "三体",
+                "刘慈欣", Arrays.asList("小说", "科幻"),
+                "", Level.ORDINARY, 10));
+        BOOKS.add(new Book("9787111075752", "设计模式",
+                "Erich Gamma", Arrays.asList("计算机与互联网", "软件工程及软件方法学", "软件工程模型"),
+                "", Level.ORDINARY, 20));
+        BOOKS.add(new Book("9787115369093", "重构：改善既有代码的设计",
+                "Martin Fowler", Arrays.asList("计算机与互联网", "软件工程及软件方法学", "软件方法"),
+                "", Level.ORDINARY, 10));
+        BOOKS.add(new Book("9787115357618", "编程珠玑",
+                "Jon Bentley", Arrays.asList("计算机与互联网", "算法与数据结构"),
+                "", Level.ORDINARY, 10));
+        BOOKS.add(new Book("9787508660752", "人类简史：从动物到上帝",
+                "Yuval Noah Harari", Arrays.asList("人文社科", "历史", "普及读物"),
+                "", Level.ORDINARY, 5));
+        BOOKS.add(new Book("9787533948108", "浮生六记",
+                "沈复", Arrays.asList("文学", "散文随笔", "小品文"),
+                "", Level.RARE, 1));
+        BOOKS.add(new Book("9787121250583", "自然语言处理综论（第二版）",
+                "Daniel Jurafsky", Arrays.asList("计算机与互联网", "自然语言处理"),
+                "", Level.ORDINARY, 3));
+
+    }
 
     private static BookDataServiceImpl singleton = new BookDataServiceImpl();
 
@@ -21,37 +45,55 @@ public class BookDataServiceImpl implements BookDataService {
 
     @Override
     public void add(Book b) {
-
+        BOOKS.add(b);
     }
 
     @Override
     public void remove(Book b) {
-
+        BOOKS.remove(b);
     }
 
     @Override
     public void update(Book b) {
-
+        BOOKS.remove(b);
+        BOOKS.add(b);
     }
 
     @Override
     public Book findByISBN(String isbn) {
+        for (Book b : BOOKS) {
+            if (isbn.equals(b.getIsbn())) {
+                return b;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Book> findByTitle(String title) {
-        return null;
+        List<Book> res = new ArrayList<>();
+        for (Book b : BOOKS) {
+            if (title.equals(b.getTitle())) {
+                res.add(b);
+            }
+        }
+        return res;
     }
 
     @Override
     public List<Book> findByAuthor(String author) {
-        return null;
+        List<Book> res = new ArrayList<>();
+        for (Book b : BOOKS) {
+            if (author.equals(b.getAuthor())) {
+                res.add(b);
+            }
+        }
+        return res;
     }
 
     @Override
     public List<Book> findAll() {
-        return null;
+        return new ArrayList<>(BOOKS);
     }
 
 }
--- assignment-3/src/main/data/impl/user/UserDataServiceImpl.java
+++ assignment-3/src/main/data/impl/user/UserDataServiceImpl.java
@@ -1,11 +1,35 @@
 package main.data.impl.user;
 
+import main.business.impl.user.PersonalInfoMediator;
 import main.common.user.*;
 import main.data.service.user.UserDataService;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UserDataServiceImpl implements UserDataService {
+
+    private static final Set<Teacher> TEACHERS = new HashSet<>();
+    private static final Set<Graduate> GRADUATES = new HashSet<>();
+    private static final Set<Undergraduate> UNDERGRADUATES = new HashSet<>();
+    private static final Set<Administrator> ADMINISTRATORS = new HashSet<>();
+
+    static {
+        TEACHERS.add(new Teacher("IN968720", "123456"));
+        TEACHERS.add(new Teacher("IN173947", "123456"));
+
+        GRADUATES.add(new Graduate("MF832042", "987654"));
+        GRADUATES.add(new Graduate("MF731325", "987654321"));
+
+        UNDERGRADUATES.add(new Undergraduate("BH734204", "543167"));
+        UNDERGRADUATES.add(new Undergraduate("BH453652", "12346123456"));
+
+        Administrator a = new Administrator("AD428201", "admin");
+        ADMINISTRATORS.add(a);
+        PersonalInfoMediator.getInstance().addAdministrator(a);
+    }
 
     private static UserDataServiceImpl singleton = new UserDataServiceImpl();
 
@@ -17,47 +41,124 @@ public class UserDataServiceImpl implements UserDataService {
 
     @Override
     public void add(Teacher u) {
-
+        TEACHERS.add(u);
     }
 
     @Override
     public void add(Graduate u) {
-
+        GRADUATES.add(u);
     }
 
     @Override
     public void add(Undergraduate u) {
-
+        UNDERGRADUATES.add(u);
     }
 
     @Override
     public void add(Administrator u) {
-
+        ADMINISTRATORS.add(u);
+        PersonalInfoMediator.getInstance().addAdministrator(u);
     }
 
     @Override
     public void add(User u) {
-
+        Class cl = u.getClass();
+        if (cl == Teacher.class) {
+            add((Teacher) u);
+        } else if (cl == Graduate.class) {
+            add((Graduate) u);
+        } else if (cl == Undergraduate.class) {
+            add((Undergraduate) u);
+        } else if (cl == Administrator.class) {
+            add((Administrator) u);
+        }
     }
 
     @Override
     public void remove(User u) {
-
+        Class cl = u.getClass();
+        if (cl == Teacher.class) {
+            TEACHERS.remove(u);
+        } else if (cl == Graduate.class) {
+            GRADUATES.remove(u);
+        } else if (cl == Undergraduate.class) {
+            UNDERGRADUATES.remove(u);
+        } else if (cl == Administrator.class) {
+            ADMINISTRATORS.remove(u);
+            PersonalInfoMediator.getInstance().removeAdministrator((Administrator) u);
+        }
     }
 
     @Override
     public void update(User u) {
-
+        Class cl = u.getClass();
+        if (cl == Teacher.class) {
+            if (TEACHERS.remove(u)) {
+                TEACHERS.add((Teacher) u);
+            }
+        } else if (cl == Graduate.class) {
+            if (GRADUATES.remove(u)) {
+                GRADUATES.add((Graduate) u);
+            }
+        } else if (cl == Undergraduate.class) {
+            if (UNDERGRADUATES.remove(u)) {
+                UNDERGRADUATES.add((Undergraduate) u);
+            }
+        } else if (cl == Administrator.class) {
+            if (ADMINISTRATORS.remove(u)) {
+                ADMINISTRATORS.add((Administrator) u);
+                PersonalInfoMediator.getInstance().removeAdministrator((Administrator) u);
+                PersonalInfoMediator.getInstance().addAdministrator((Administrator) u);
+            }
+        }
     }
 
     @Override
     public User findByUsername(String username) {
+        for (Teacher u : TEACHERS) {
+            if (username.equals(u.getUsername())) {
+                return u;
+            }
+        }
+        for (Graduate u : GRADUATES) {
+            if (username.equals(u.getUsername())) {
+                return u;
+            }
+        }
+        for (Undergraduate u : UNDERGRADUATES) {
+            if (username.equals(u.getUsername())) {
+                return u;
+            }
+        }
+        for (Administrator u : ADMINISTRATORS) {
+            if (username.equals(u.getUsername())) {
+                return u;
+            }
+        }
         return null;
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        List<User> res = new ArrayList<>();
+        res.addAll(TEACHERS);
+        res.addAll(GRADUATES);
+        res.addAll(UNDERGRADUATES);
+        res.addAll(ADMINISTRATORS);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Set<User> s = new HashSet<>();
+        s.add(new Teacher("IN968720", "123456"));
+        s.add(new Teacher("IN173947", "123456"));
+        s.add(new Graduate("MF832042", "987654"));
+        s.add(new Graduate("MF731325", "987654321"));
+        System.out.println(s);
+        System.out.println(s.remove(new Graduate("MF731325", "")));
+        System.out.println(s.remove(new Graduate("MF000000", "")));
+        s.add(new Graduate("MF731325", "000000"));
+        System.out.println(s);
     }
 
 }
